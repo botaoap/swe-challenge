a4f7e29481c23608db80b98bfb044e43e93555e0
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-09 02:04:20 -0300

    implement splash screen on ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    @State private var showSplash = true
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            
+            if showSplash {
+                
+            }
         }
     }
 }
