86fdbe90e771f92cad064dca6f1d0672cbf0070c
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-09 01:22:49 -0300

    create use case to get from data store

---
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/domain/usecase/GetLoggedInStatusUseCase.kt
@@ -0,0 +1,47 @@
+package com.gabrielbotao.swechallenge.domain.usecase
+
+import com.gabrielbotao.swechallenge.domain.repository.Repository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+interface GetLoggedInStatusUseCase {
+    fun execute(): Flow<LoggedInState>
+}
+
+class GetLoggedInStatusUseCaseImpl(
+    private val repository: Repository,
+    private val coroutineDispatcher: CoroutineDispatcher
+) : GetLoggedInStatusUseCase {
+    override fun execute(): Flow<LoggedInState> = flow {
+        try {
+            repository.getCurrentLoggedInStatus().map { isLoggedIn ->
+                isLoggedIn
+            }.let { response ->
+                response.collect { value ->
+                    println("Key isLogged(GetLoggedInStatusUseCase): $value")
+                    emit(LoggedInState.Success(value))
+                }
+            }
+        } catch (e: Exception) {
+            e
+            println("Key isLogged(GetLoggedInStatusUseCase): Error $e")
+            emit(LoggedInState.Error)
+        }
+    }.onStart {
+        println("Key isLogged(GetLoggedInStatusUseCase): Loading")
+        emit(LoggedInState.Loading)
+    }.flowOn(coroutineDispatcher)
+}
+
+sealed class LoggedInState {
+    data object Loading : LoggedInState()
+    data class Success(
+        val isLoggedIn: Boolean
+    ) : LoggedInState()
+
+    data object Error : LoggedInState()
+}
