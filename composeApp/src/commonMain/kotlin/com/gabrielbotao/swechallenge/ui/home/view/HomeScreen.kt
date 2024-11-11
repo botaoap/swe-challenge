b25491dc8f44664b26cb76ad9c35d19ce973a33c
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-11 19:22:18 -0300

    create compose layout of home

--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/ui/home/view/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/ui/home/view/HomeScreen.kt
@@ -1,21 +1,169 @@
+@file:OptIn(KoinExperimentalAPI::class)
+
 package com.gabrielbotao.swechallenge.ui.home.view
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.rememberAsyncImagePainter
+import com.gabrielbotao.swechallenge.domain.model.CategoryGroup
+import com.gabrielbotao.swechallenge.ui.home.uistate.ProductsUIState
+import com.gabrielbotao.swechallenge.ui.home.viewmodel.HomeViewModel
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
+    val viewModel = koinViewModel<HomeViewModel>()
+    val dataState = viewModel.productsState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.getProducts()
+    }
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        dataState.value.let { state ->
+            when (state) {
+                ProductsUIState.Loading -> Loading(modifier)
+
+                is ProductsUIState.Success -> CategorizedLazeColumn(state.categories)
+
+                ProductsUIState.Error -> Error(modifier, viewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun Loading(modifier: Modifier) {
+    Column(
         modifier = modifier
             .fillMaxSize(),
-        contentAlignment = Alignment.Center,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = "Home Screen")
+        CircularProgressIndicator(
+            color = Color.Gray,
+            strokeWidth = 2.dp
+        )
+        Text("Loading...")
+    }
+}
+
+@Composable
+fun Error(modifier: Modifier, viewModel: HomeViewModel) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Error")
+        Button(
+            onClick = {
+                viewModel.getProducts()
+            }
+        ) {
+            Text("Try again")
+        }
+    }
+}
+
+@Composable
+fun CategoryHeader(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = Color.White,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.Gray)
+            .padding(16.dp)
+    )
+}
+
+@Composable
+fun CategoryItem(
+    text: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.background)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = imageUrl),
+            contentDescription = "description",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .requiredSize(64.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(color = Color.LightGray)
+        )
+
+        Text(
+            text = text,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun CategorizedLazeColumn(
+    categories: List<CategoryGroup>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        categories.forEach { category ->
+            stickyHeader {
+                CategoryHeader(category.title)
+            }
+            items(category.item) { list ->
+                CategoryItem(list.title, list.thumbnail)
+            }
+        }
     }
 }
