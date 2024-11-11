77767e937c91fd315d5d7e32c8ac5f7b78d172e0
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-11 16:11:42 -0300

    change injection to slinge

--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/di/Modules.kt
@@ -1,9 +1,12 @@
 package com.gabrielbotao.swechallenge.di
 
 import com.gabrielbotao.swechallenge.data.repository.RepositoryImpl
+import com.gabrielbotao.swechallenge.domain.mapper.LoginMapper
 import com.gabrielbotao.swechallenge.domain.repository.Repository
 import com.gabrielbotao.swechallenge.domain.usecase.GetLoggedInStatusUseCase
 import com.gabrielbotao.swechallenge.domain.usecase.GetLoggedInStatusUseCaseImpl
+import com.gabrielbotao.swechallenge.domain.usecase.PostLoginUseCase
+import com.gabrielbotao.swechallenge.domain.usecase.PostLoginUseCaseImpl
 import com.gabrielbotao.swechallenge.domain.usecase.SaveLoggedInStatusUseCase
 import com.gabrielbotao.swechallenge.domain.usecase.SaveLoggedInStatusUseCaseImpl
 import com.gabrielbotao.swechallenge.ui.bottomnavigation.viewmodel.MainViewModel
@@ -14,6 +17,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 expect val platformModule: Module
@@ -22,26 +26,34 @@ val shareModule = module {
     getRepositoryModule(this)
     getUseCaseModule(this)
     getViewModelModule(this)
+    getMapperModule(this)
 }
 
 private fun getRepositoryModule(module: Module) {
     with(module) {
-        factory<Repository> { RepositoryImpl(get()) }
+        single<Repository> { RepositoryImpl(get(), get()) }
     }
 }
 
 private fun getUseCaseModule(module: Module) {
     with(module) {
-        factory<GetLoggedInStatusUseCase> { GetLoggedInStatusUseCaseImpl(get(), Dispatchers.IO) }
-        factory<SaveLoggedInStatusUseCase> { SaveLoggedInStatusUseCaseImpl(get()) }
+        single<GetLoggedInStatusUseCase> { GetLoggedInStatusUseCaseImpl(get(), Dispatchers.IO) }
+        single<SaveLoggedInStatusUseCase> { SaveLoggedInStatusUseCaseImpl(get()) }
+        single<PostLoginUseCase> { PostLoginUseCaseImpl(get(), get(), Dispatchers.IO) }
     }
 }
 
 private fun getViewModelModule(module: Module) {
     with(module) {
-        factoryOf(::MainViewModel)
-        factoryOf(::InitialViewModel)
-        factoryOf(::LoginViewModel)
-        factoryOf(::ProfileViewModel)
+        singleOf(::MainViewModel)
+        singleOf(::InitialViewModel)
+        singleOf(::LoginViewModel)
+        singleOf(::ProfileViewModel)
+    }
+}
+
+private fun getMapperModule(module: Module) {
+    with(module) {
+        factoryOf(::LoginMapper)
     }
 }
