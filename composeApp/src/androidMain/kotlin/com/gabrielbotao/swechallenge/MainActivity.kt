9b0b4684f7a68ccc2e254897d73b4bfd4104c313
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-11 16:14:01 -0300

    implement login on android native to know when login or logout

--- composeApp/src/androidMain/kotlin/com/gabrielbotao/swechallenge/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gabrielbotao/swechallenge/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import com.gabrielbotao.swechallenge.util.Util
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -27,8 +31,24 @@ class MainActivity : ComponentActivity() {
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
                 View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        lifecycle.addObserver(ActivityLifecycleObserver())
+
         setContent {
             App()
         }
     }
+}
+
+class ActivityLifecycleObserver : LifecycleEventObserver {
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_DESTROY -> {
+                Util.resetCountOnDestroy = 0
+            }
+
+            else -> {}
+        }
+    }
+
 }
---
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/util/Util.kt
@@ -0,0 +1,5 @@
+package com.gabrielbotao.swechallenge.util
+
+object Util {
+    var resetCountOnDestroy: Int = 0
+}
