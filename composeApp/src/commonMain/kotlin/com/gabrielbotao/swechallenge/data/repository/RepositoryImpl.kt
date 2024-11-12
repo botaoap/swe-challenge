c44552204f1803f1e3e5e3a8b6be2ae6e51d61be
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-12 13:29:15 -0300

    implement data store to save user data

--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/data/repository/RepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/data/repository/RepositoryImpl.kt
@@ -2,9 +2,14 @@ package com.gabrielbotao.swechallenge.data.repository
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import com.gabrielbotao.swechallenge.constants.DataStoreKey.EMAIL_KEY
+import com.gabrielbotao.swechallenge.constants.DataStoreKey.FULL_NAME_KEY
+import com.gabrielbotao.swechallenge.constants.DataStoreKey.IS_LOGGED_IN_KEY
+import com.gabrielbotao.swechallenge.constants.DataStoreKey.PROFILE_IMAGE_KEY
+import com.gabrielbotao.swechallenge.constants.DataStoreKey.USERNAME_KEY
 import com.gabrielbotao.swechallenge.data.remote.api.WebService
+import com.gabrielbotao.swechallenge.domain.model.LoginModel
 import com.gabrielbotao.swechallenge.domain.repository.Repository
 import io.ktor.client.statement.*
 import kotlinx.coroutines.flow.Flow
@@ -14,20 +19,38 @@ class RepositoryImpl(
     private val webService: WebService,
     private val dataStore: DataStore<Preferences>
 ) : Repository {
-    private companion object {
-        val IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
-    }
 
     override suspend fun getCurrentLoggedInStatus(): Flow<Boolean> =
         run {
             dataStore.data.map { preferences ->
-                preferences[IS_LOGGED_IN] ?: false
+                preferences[IS_LOGGED_IN_KEY] ?: false
             }
         }
 
     override suspend fun saveLoggedInStatus(isLoggedIn: Boolean) {
         dataStore.edit { preferences ->
-            preferences[IS_LOGGED_IN] = isLoggedIn
+            preferences[IS_LOGGED_IN_KEY] = isLoggedIn
+        }
+    }
+
+    override suspend fun getCurrentUserData(): Flow<LoginModel> =
+        run {
+            dataStore.data.map { preferences ->
+                LoginModel(
+                    username = preferences[USERNAME_KEY] ?: "",
+                    fullName = preferences[FULL_NAME_KEY] ?: "",
+                    email = preferences[EMAIL_KEY] ?: "",
+                    image = preferences[PROFILE_IMAGE_KEY] ?: ""
+                )
+            }
+        }
+
+    override suspend fun saveUserData(data: LoginModel) {
+        dataStore.edit { preferences ->
+            preferences[USERNAME_KEY] = data.username
+            preferences[FULL_NAME_KEY] = data.fullName
+            preferences[EMAIL_KEY] = data.email
+            preferences[PROFILE_IMAGE_KEY] = data.image
         }
     }
 
--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/domain/repository/Repository.kt
@@ -1,5 +1,6 @@
 package com.gabrielbotao.swechallenge.domain.repository
 
+import com.gabrielbotao.swechallenge.domain.model.LoginModel
 import io.ktor.client.statement.*
 import kotlinx.coroutines.flow.Flow
 
@@ -10,11 +11,17 @@ interface Repository {
     /** This function save status of is_logged_in key in DataStore */
     suspend fun saveLoggedInStatus(isLoggedIn: Boolean)
 
+    /** This function get data of username, full_name, email, profile_image key in DataStore */
+    suspend fun getCurrentUserData(): Flow<LoginModel>
+
+    /** This function save data of username, full_name, email, profile_image key in DataStore */
+    suspend fun saveUserData(data: LoginModel)
+
     /** This function login in app */
     suspend fun postLogin(
         username: String,
         password: String
-    ) : HttpResponse
+    ): HttpResponse
 
     /** This function get list of products */
     suspend fun getProducts(): HttpResponse
