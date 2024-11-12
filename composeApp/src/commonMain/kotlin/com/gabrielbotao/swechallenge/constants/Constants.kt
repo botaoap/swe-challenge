d1f6c196d20b8ead651d047894ccd63760ba3cee
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-12 13:30:13 -0300

    create keys to data store

--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/constants/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/constants/Constants.kt
@@ -1,5 +1,16 @@
 package com.gabrielbotao.swechallenge.constants
 
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+
 object UrlProvider {
     const val BASE_URL = "https://dummyjson.com"
+}
+
+object DataStoreKey {
+    val USERNAME_KEY = stringPreferencesKey("username")
+    val FULL_NAME_KEY = stringPreferencesKey("full_name")
+    val EMAIL_KEY = stringPreferencesKey("email")
+    val PROFILE_IMAGE_KEY = stringPreferencesKey("profile_image")
+    val IS_LOGGED_IN_KEY = booleanPreferencesKey("is_logged_in")
 }
