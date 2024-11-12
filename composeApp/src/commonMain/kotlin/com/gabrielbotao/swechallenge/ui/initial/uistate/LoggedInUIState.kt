df368862c56d22cc58611a3d98669a3828876507
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-12 00:21:22 -0300

    rearrange code ui state

--- composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/ui/initial/uistate/LoggedInUIState.kt
+++ composeApp/src/commonMain/kotlin/com/gabrielbotao/swechallenge/ui/initial/uistate/LoggedInUIState.kt
@@ -1,14 +1,16 @@
 package com.gabrielbotao.swechallenge.ui.initial.uistate
 
 sealed class LoggedInUIState {
-    data object Loading: LoggedInUIState()
+    data object Loading : LoggedInUIState()
     data class Success(
         val isLoggedIn: Boolean
-    ): LoggedInUIState()
-    data object Error: LoggedInUIState()
+    ) : LoggedInUIState()
+
+    data object Error : LoggedInUIState()
 }
 
 sealed class LoggedInGoToFlow {
-    data object GoToMainScreen: LoggedInGoToFlow()
-    data object GoToLoginScreen: LoggedInGoToFlow()
+    data object Loading : LoggedInGoToFlow()
+    data object GoToMainScreen : LoggedInGoToFlow()
+    data object GoToLoginScreen : LoggedInGoToFlow()
 }
