8e347feb551ce985f778ceebd2ba09fe4d965795
Gabriel Botao <gabriel.botao@br.experian.com>
2024-11-12 00:17:05 -0300

    remove delay from splash screen

--- composeApp/src/androidMain/kotlin/com/gabrielbotao/swechallenge/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gabrielbotao/swechallenge/MainActivity.kt
@@ -9,25 +9,13 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
 import com.gabrielbotao.swechallenge.util.Util
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private var isChecking: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch {
-            delay(1500)
-            isChecking = false
-        }
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                isChecking
-            }
-        }
+        installSplashScreen()
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
         window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
                 View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
